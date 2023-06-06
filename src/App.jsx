32e03a2e214c634d0ae6ac332d692251314b327b
NiNiShyuneLae <heathersan865@gmail.com>
2023-06-06 10:03:54 +0630

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,9 @@ import Loading from './pages/Loading'
 const App = () => {
   return (
     <div>
-      {/* <div className="px-6 py-1 hover:bg-primary hover:text-white transition-all duration-200 rounded-full border-2 border-primary w-96 text-center uppercase tracking-wider font-semibold  text-primary">Login</div> */}
       <Routes>
-        {/* <Route path='/' element={<Dashboard/>}/>
-        <Route path='*' element={<NotFound/>}/> */}
-        <Route path='/' element={<Loading/>}/>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
       </div>
   )
